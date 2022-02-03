fe5a20120d674635cdf9ee398fcbbb8a9b7d3e28
Victor Portugal <victorvasconcellos5@hotmail.com>
2022-02-02 21:41:40 -0300

    Median solution

--- median.js
+++ median.js
@@ -5,10 +5,20 @@ const main = () => {
         return sum / (arr1.length + arr2.length);
     }
 
+    const findMedianSortedArrays = (arr1, arr2) => {
+        const arr = [...arr1, ...arr2].sort();
+        console.log(arr);
+        const median = arr.length%2 == 1 ? arr[(arr.length - 1)/2] : (arr[arr.length/2 - 1] + arr[arr.length/2])/2;
+        console.log( arr[(arr.length - 1)/2 ]);
+        
+        return median;
+    }
+
     const arr1 = [1, 3];
     const arr2 = [2, 7];
 
     console.log(findAverageSortedArrays(arr1, arr2));
+    console.log(findMedianSortedArrays([1, 3], [2]));
 }
 
 main();
